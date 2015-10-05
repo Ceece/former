3c75863400ad8d8527deb8c2a87b4518ae2ae9ad
Teerapong Manakul <imumi@msn.com>
2015-10-05 13:45:35 +0700

    change action

--- src/Former/Form/Form.php
+++ src/Former/Form/Form.php
@@ -190,7 +190,7 @@ class Form extends FormerObject
 	 */
 	public function action($action)
 	{
-		$this->action = $action ? $this->url->to($action, array(), $this->secure) : null;
+		$this->action = $action;
 
 		return $this;
 	}
